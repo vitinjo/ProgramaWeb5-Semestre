c413f69d9d2c0d6f7218a1a15530258dae3ec2dd
VITOR BITTENCOURT DOS SANTOS <vitor.bittencourt@a.ucb.br>
2025-02-20 21:08:10 -0300

    Estrutura for e if

--- prog3.js
+++ prog3.js
@@ -2,5 +2,15 @@
 var valor = Number (prompt("Digite um valor "));
 
 for (i=0; i<valor; i++){
-    document.write("Valor atual  " + i+ "<br");
+    //document.write("Valor atual  " + i + "<br>");
+
+    if (i % 2 == 0){
+    document.write("<p style='color:orange;'>" +i+ "</p>")
+
+    }else{
+
+        document.write("<p style='color:blue;'>" +i+ "</p>")
+
+    }
+
 }
